e46a5f6ea1316181ad7bff509687778b03219112
dawdawfawf <ipalamarchuk905@gmail.com>
2026-06-07 21:07:25 +0300

    Set base path for Vite configuration to '/portfolio/'

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import { defineConfig } from "vite";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
